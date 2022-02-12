025bcc0d3f8b7dee6c1b9dc3215c5cbad154fb70
JaTin-at-git <80684191+JaTin-at-git@users.noreply.github.com>
2022-02-12 09:04:37 +0530

    second commit

--- js/jquerry.js
+++ js/jquerry.js
@@ -3,10 +3,13 @@ $(window).scroll(function () {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = 50;
 
-    if (y_scroll_pos > scroll_pos_test) {
-        $(".navbar").css("background-color", "var(--color-dark)");
-    } else {
-        $(".navbar").css("background-color", "transparent");
+    if (window.outerWidth > 900) {
+        if (y_scroll_pos > scroll_pos_test) {
+            $(".navbar").css("background-color", "var(--color-dark)");
+        } else {
+            $(".navbar").css("background-color", "transparent");
+        }
     }
-    console.log($("navbar"));
+    console.log(window.innerWidth);
+    console.log(window.outerWidth);
 });
