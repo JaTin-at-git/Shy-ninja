9d6e74c830a5b8b556404189c1aa4aaddffbe007
JaTin-at-git <80684191+JaTin-at-git@users.noreply.github.com>
2022-02-12 08:16:07 +0530

    second commit

--- js/jquerry.js
+++ js/jquerry.js
@@ -1,12 +1,12 @@
-// //change the navbar background on certail scroll
-// $(window).scroll(function () {
-//     var y_scroll_pos = window.pageYOffset;
-//     var scroll_pos_test = 100;
-//
-//     if (y_scroll_pos > scroll_pos_test) {
-//         $(".navbar").css("background-color", "var(--color-dark)");
-//     } else {
-//         $(".navbar").css("background-color", "transparent");
-//     }
-//     console.log($("navbar"));
-// });
+//change the navbar background on certail scroll
+$(window).scroll(function () {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = 50;
+
+    if (y_scroll_pos > scroll_pos_test) {
+        $(".navbar").css("background-color", "var(--color-dark)");
+    } else {
+        $(".navbar").css("background-color", "transparent");
+    }
+    console.log($("navbar"));
+});
